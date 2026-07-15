ceebe48b8885ef8cf61eadf4bbd2d057af2dad47
Uticu <strugar.ma.maxim@student.utcluj.ro>
2026-07-15 10:09:27 +0300

    Initial commit: Setare structura Monorepo si scripturi DB

---
+++ database/erp_management_system.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS OrderDetails;
+DROP TABLE IF EXISTS Bill;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Client;
+
+CREATE TABLE Products(
+    productID INT AUTO_INCREMENT PRIMARY KEY,
+    productName VARCHAR(255) NOT NULL,
+    productPrice DECIMAL(10, 2) NOT NULL,
+    productStock INT NOT NULL
+);
+
+CREATE TABLE Client(
+    clientID INT AUTO_INCREMENT PRIMARY KEY,
+    clientName VARCHAR(255) NOT NULL,
+    clientEmail VARCHAR(255) NOT NULL,
+    clientAddress VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Orders(
+    orderID INT AUTO_INCREMENT PRIMARY KEY,
+    clientID INT NOT NULL,
+    orderDate DATE NOT NULL,
+    orderStatus VARCHAR(255) NOT NULL,
+    orderDeliveryAddress VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_Orders_Client
+        FOREIGN KEY (clientID) REFERENCES Client(clientID)
+);
+
+CREATE TABLE Bill(
+    billID INT AUTO_INCREMENT PRIMARY KEY,
+    orderID INT NOT NULL,
+    billIssueDate DATE NOT NULL,
+    billSeries VARCHAR(3) NOT NULL,
+    billNumber VARCHAR(5) NOT NULL,
+    CONSTRAINT fk_Bill_Order
+        FOREIGN KEY (orderID) REFERENCES Orders(orderID)
+);
+
+CREATE TABLE OrderDetails(
+    orderDetailsID INT AUTO_INCREMENT PRIMARY KEY,
+    orderID INT NOT NULL,
+    productID INT NOT NULL,
+    orderDetailsQuantity INT NOT NULL,
+    sellingPriceAtTheMoment DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT fk_OrderDetails_Orders
+        FOREIGN KEY (orderID) REFERENCES Orders(orderID),
+    CONSTRAINT fk_OrderDetails_Products
+        FOREIGN KEY (productID) REFERENCES Products(productID)
+);
+
+
